c7926f4710204969994d8ab6a9ba1630c9b6eaa1
David3D-AndweM <127220902+David3D-AndweM@users.noreply.github.com>
2023-10-04 12:26:32 +0200

    Update The-ItalianResto_Delight.py

--- extract_and_structure-data.py/The-ItalianResto_Delight.py
+++ extract_and_structure-data.py/The-ItalianResto_Delight.py
@@ -2,7 +2,7 @@
 
 import re
 
-# data for the Italian Delight restaurant
+# data for the Italian Delight restauran
 restaurant_data = """
 Restaurant: Italian Delight - Italian
 Ingredients: pasta, tomatoes, cheese, basil
